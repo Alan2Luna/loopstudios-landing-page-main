aa0893cd21c6e36fbd0cbde85a3c5b354c121898
Alan Luna <aklt1994@gmail.com>
2021-05-06 01:48:55 +0200

    mobile finished

--- js/index.js
+++ js/index.js
@@ -2,12 +2,19 @@ const buttonHamburger = document.querySelector('.button__hamburger')
 const img = document.querySelector('.button__hamburger-img')
 const nav = document.querySelector('.nav')
 
-buttonHamburger.addEventListener('click', () => {
+const openNavBar = () => {
     nav.classList.toggle('nav-mobile-active')
 
     if(nav.classList.contains('nav-mobile-active')) {
         img.src = "./images/icon-close.svg"
+        document.body.style.overflowY = "hidden";
+        return
     } else {
         img.src = "./images/icon-hamburger.svg"
+        document.body.style.overflowY = "initial";
+        return
     }
-})
+}
+
+
+buttonHamburger.addEventListener('click', openNavBar)
